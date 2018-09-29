c04369e55b58d43b661e71c3d9b3b9a735561546
@tishushu <tishushu@SpeedyBubo.home>
2018-09-29 01:14:36 -0400

    added functions to game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push( cardOne );
-var cardTwo = cards[2];
-cardsInPlay.push( cardTwo );
-console.log("User flipped " + cardOne + "!");
-console.log("User flipped " + cardTwo + "!");
-if (cardsInPlay.length === 2){
+
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You've found a match!");
 	} else {
-		alert("Sorry, loser... Try again...")
+		alert("Sorry, loser... Try again...");
 	}
 }
+
+var flipCard = function( cardId ){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push[cardId];
+var cardTwo = cards[2];
+  if (cardsInPlay.length === 2){
+	checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
+

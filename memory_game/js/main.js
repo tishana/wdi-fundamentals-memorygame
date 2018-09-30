70b19e755ebe0f1966349428f8743b31aea52165
@tishushu <tishushu@SpeedyBubo.home>
2018-09-30 13:49:02 -0400

    added objects to game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+  },
+  {
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImages: "images/queen-of-diamonds.png"
+  },
+  {
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage: "images/king-of-hearts.png"
+  },
+  {
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -10,8 +31,10 @@ var checkForMatch = function(){
 }
 
 var flipCard = function( cardId ){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push[cardId];
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 var cardTwo = cards[2];
   if (cardsInPlay.length === 2){
 	checkForMatch();
